6f2fa3ea779b328f495dd9409a4f224f510089a5
carlynorama <carlynorama@gmail.com>
2025-06-25 10:55:14 -0700

    tag pages done, start rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:"https://bucolic-churros-68e989.netlify.app"
+});
